80e0d428e5c0423635696451758e6e06a0bd93d9
Qu4k <pippo.reds@gmail.com>
2020-08-25 11:09:02 +0200

    fix: mod.ts exports

--- mod.ts
+++ mod.ts
@@ -1,10 +1,3 @@
 // Copyright 2020-present the denosaurs team. All rights reserved. MIT license.
 
-import { setup, prefix } from "./branch.ts";
-
-export { LogConfig } from "./branch.ts";
-
-export default {
-  setup,
-  prefix,
-};
+export { LogConfig, setup, prefix } from "./branch.ts";
